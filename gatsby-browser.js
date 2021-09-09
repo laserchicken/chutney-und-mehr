8d7e2504670770267662ca811d89831e00e82355
laserchicken <mateusz.kmiecinski@yahoo.com>
2021-09-09 13:53:36 +0200

    add intro component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import "@fontsource/montserrat";
-import "@fontsource/montserrat/300.css";
 import "@fontsource/montserrat/200.css";
+import "@fontsource/montserrat/300.css";
 import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/700.css";
 import "@fontsource/montserrat/800.css";
--- src/components/homepageContainer.js
+++ src/components/homepageContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import Masthead from "./masthead";
 import Tiles from "./tiles";
 import Insta from "./insta";
+import Intro from "./intro";
 import Footer from "./footer";
 import styles from "./homepageContainer.module.css";
 
@@ -9,6 +10,7 @@ export default function HomepageContainer() {
   return (
     <div>
       <Masthead />
+      <Intro />
       <Tiles />
       <Footer />
     </div>
