694a7340d5479e8fd44edd0d4cbc2780435eb1a5
laserchicken <mateusz.kmiecinski@yahoo.com>
2021-09-14 17:59:36 +0200

    update catering component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,4 @@ import "@fontsource/montserrat/300.css";
 import "@fontsource/montserrat/500.css";
 import "@fontsource/montserrat/700.css";
 import "@fontsource/montserrat/800.css";
+import "react-image-gallery/styles/css/image-gallery.css";
--- src/components/catering.js
+++ src/components/catering.js
@@ -1,120 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "gatsby";
-import ImageTile from "./imageTile";
-import TitleTile from "./titleTile";
-import chutneyundmehrMenu from "../downloads/chutneyundmehr-menu.pdf";
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import salat from "../images/salat.jpg";
+import fingerfood from "../images/fingerfood.jpg";
+import glas from "../images/glas.jpg";
 import * as styles from "./catering.module.css";
-import * as tileStyles from "./tile.module.css";
+
+const images = [
+  {
+    original: glas,
+  },
+  {
+    original: fingerfood,
+  },
+  {
+    original: salat,
+  },
+];
 
 export default function Catering() {
-  const [hovered, setHovered] = useState();
-
   return (
     <div id="catering" className={styles.wrapper}>
-      <div className={styles.header}>CATERING</div>
-      <div className={styles.separator} />
-      <div className={styles.textWrapper}>
-        <p className={styles.textBlock}>
-          Kochen ist meine Leidenschaft, die ich mit Ihnen teilen möchte. Ich
-          lasse mich von Aromen inspirieren, die ich auf meinen Reisen um die
-          Welt kennen gelernt habe, z. B. in Thailand, Kambodscha und Vietnam.
-          Ich biete Ihnen eine Fusionsküche, die moderne internationale Küche
-          mit regionalen, saisonalen Produkten kombiniert. Ich gehe jeden
-          Auftrag mit dem größten Engagement an.
-          <br />
-          <br />
-          Alle Gerichte werden nach meinen eigenen Rezepten zubereitet, die sich
-          von denen der lokalen Catering-Unternehmen abheben. Wann immer
-          möglich, komponiere ich sie mit meinen Original CHUTNEY UND MEHR
-          Chutneys, deren Sortiment Sie auf der Homepage sehen können.
-        </p>
-        <p className={styles.textBlock}>
-          Die höchste Qualität der lokalen Produkte und eine innovative,
-          originelle und raffinierte Art des Servierens stellen auch die
-          anspruchsvollsten Kunden zufrieden. Ich bemühe mich, dass alle meine
-          Snacks und Gerichte perfekt zusammenpassen.
-          <br />
-          <br />
-          Mein Motto lautet #enjoy the moment und genau das möchte ich Ihnen
-          servieren: unglaubliche Geschmackserlebnisse und die Freude am Essen.
-          <br />
-          <br />
-          Ich bin bereit, Ihnen jeden Catering-Traum zu erfüllen, von einer
-          Gartenparty bis zu einem intimen Familientreffen.
-        </p>
-      </div>
-
-      <div className={styles.tiles}>
-        <Link
-          className={`${styles.row}`}
-          to="/pflaumen-chutney-mit-walnussen"
-          onMouseEnter={(_) => {
-            setHovered("glas");
-          }}
-          onMouseLeave={(_) => {
-            setHovered(null);
-          }}
-        >
-          <ImageTile image={"glas"} className={styles.oneHalf} />
-          <TitleTile
-            header={"glas"}
-            subheader={
-              "GRÜNES ERBSENMOUSSE I RÄUCHERLACHS<br/>griechischer joghurt I mandelblättchen I dill"
-            }
-            backgroundColor={"green"}
-            hovered={hovered === "glas"}
-            className={styles.oneHalf}
-            styles={tileStyles}
-            button={"menu"}
-          />
-        </Link>
-        <Link
-          className={`${styles.row} ${styles.rowReverse}}`}
-          to="/tomaten-mango-chutney"
-          onMouseEnter={(_) => {
-            setHovered("fingerfood");
-          }}
-          onMouseLeave={(_) => {
-            setHovered(null);
-          }}
-        >
-          <TitleTile
-            header={"fingerfood"}
-            backgroundColor={"green"}
-            subheader={
-              "ROTE ZWIEBEL-CHUTNEY I FETA<br/> muffins aus filo-teig I schnittlauch"
-            }
-            hovered={hovered === "fingerfood"}
-            styles={tileStyles}
-            className={styles.oneHalf}
-            button={"menu"}
-          />
-          <ImageTile image={"fingerfood"} className={styles.oneHalf} />
-        </Link>
-        <Link
-          className={`${styles.row}`}
-          to="/rote-zwiebel-chutney-mit-haselnussen"
-          onMouseEnter={(_) => {
-            setHovered("salad");
-          }}
-          onMouseLeave={(_) => {
-            setHovered(null);
-          }}
-        >
-          <ImageTile image={"salat"} className={styles.oneHalf} />
-          <TitleTile
-            header={"SALATSCHÜSSEL"}
-            subheader={
-              "ZUCCHINI I ROQUEFORT<br/>dunkle weintrauben I radieschen I walnüsse I petersilie"
-            }
-            backgroundColor={"green"}
-            hovered={hovered === "salad"}
-            className={styles.oneHalf}
-            styles={tileStyles}
-            button={"menu"}
-          />
-        </Link>
-      </div>
+      <ImageGallery
+        showFullscreenButton={false}
+        showPlayButton={false}
+        slideDuration={0}
+        slideInterval={2000}
+        items={images}
+      />
     </div>
   );
 }
